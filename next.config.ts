3885211c10834dd16262590c0906b8c7efec4d14
Pouria Delfanazari <pdelfan@gmail.com>
2025-07-17 16:15:17 -0700

    chore: .md support

--- next.config.ts
+++ next.config.ts
@@ -28,7 +28,7 @@ const nextConfig: NextConfig = {
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
 });
 
-export default withMDX(nextConfig)
-
+export default withMDX(nextConfig);
