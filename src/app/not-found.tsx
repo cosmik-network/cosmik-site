6e393988d0763b1524c2d30709194c551812ca3d
Pouria Delfanazari <pdelfan@gmail.com>
2025-07-17 11:23:14 -0700

    feat: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Button, Center, Container, Stack, Title } from "@mantine/core";
+import { FaArrowRight } from "react-icons/fa";
+
+export default function NotFound() {
+  return (
+    <Container>
+      <Center h={"65svh"}>
+        <Stack align="center">
+          <Title order={1}>Page not found</Title>
+          <Button component="a" href="/" variant="light" rightSection={<FaArrowRight />}>
+            Go to home page
+          </Button>
+        </Stack>
+      </Center>
+    </Container>
+  );
+}
