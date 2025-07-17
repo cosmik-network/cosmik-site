734b067056e8fbea0695a3d8752e57ebda1a3592
Pouria Delfanazari <pdelfan@gmail.com>
2025-07-17 15:02:26 -0700

    chore: noindex response header

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
+  },
 };
 
 export default nextConfig;
