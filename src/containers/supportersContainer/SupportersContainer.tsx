4d12bc0d48f1d6d692acc2876cf76508e63fa548
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-05 11:35:13 -0700

    feat: add links for supporters

--- src/containers/supportersContainer/SupportersContainer.tsx
+++ src/containers/supportersContainer/SupportersContainer.tsx
@@ -4,17 +4,26 @@ export default function SupportersContainer() {
   return (
     <Stack align="center">
       <Group justify="center">
-        <Image src={"/images/astera.svg"} alt={"Astera logo"} h={37} w={132} />
-        <Image
-          src={"/images/open-philanthropy.svg"}
-          alt={"Open Philanthropy logo"}
-          h={37}
-          w={132}
-        />
+        <Anchor href="http://astera.org/" target="_blank">
+          <Image
+            src={"/images/astera.svg"}
+            alt={"Astera logo"}
+            h={37}
+            w={132}
+          />
+        </Anchor>
+        <Anchor href="https://www.openphilanthropy.org/" target="_blank">
+          <Image
+            src={"/images/open-philanthropy.svg"}
+            alt={"Open Philanthropy logo"}
+            h={37}
+            w={132}
+          />
+        </Anchor>
       </Group>
       <Text>
         Cosmik is fiscally sponsored by{" "}
-        <Anchor href="https://www.homeworld.bio/" fw={600}>
+        <Anchor href="https://www.homeworld.bio/" target="_blank" fw={600}>
           Homeworld Collective
         </Anchor>
       </Text>
