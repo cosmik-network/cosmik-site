78c1764d8dc2958231f2187d0c9ef7897fe0bcf9
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-08 15:23:48 -0700

    feat: project card

--- src/containers/projectsContainer/ProjectsContainer.tsx
+++ src/containers/projectsContainer/ProjectsContainer.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import ProjectCard from "@/components/contentDisplay/projectCard/ProjectCard";
-import { Group, Stack, Tabs, Text } from "@mantine/core";
+import { Stack, Tabs, Text } from "@mantine/core";
+import SembleScreenshot from "@/assets/images/semble-screenshot.png";
+import HyperfeedScreenshot from "@/assets/images/hyperfeed-screenshot.png";
 
 export default function ProjectsContainer() {
   return (
     <Stack align="center">
       <Tabs variant="pills" radius="xl" defaultValue="ongoing">
-        <Tabs.List justify="center">
+        <Tabs.List justify="center" mb={"lg"}>
           <Tabs.Tab value="ongoing">
             <Text fz={"sm"} fw={600}>
               Ongoing
@@ -20,28 +22,30 @@ export default function ProjectsContainer() {
           </Tabs.Tab>
         </Tabs.List>
 
-        <Group justify="center" align="stretch" mt={"lg"}>
-          <Tabs.Panel value="ongoing">
-            <ProjectCard
-              name="Semble"
-              description="Social bookmarking for intentional browsing."
-              logo="/images/semble-logo.png"
-              year={2025}
-              status="ongoing"
-              githubLink="https://github.com/cosmik-network/semble"
-            />
-          </Tabs.Panel>
-          <Tabs.Panel value="archived">
-            <ProjectCard
-              name="Hyperfeed"
-              description="Next-Generation Research Discovery."
-              logo="/images/hyperfeed-logo.png"
-              year={2024}
-              status="archived"
-              githubLink="https://github.com/Common-SenseMakers/sensemakers"
-            />
-          </Tabs.Panel>
-        </Group>
+        <Tabs.Panel value="ongoing">
+          <ProjectCard
+            name="Semble"
+            description="Social bookmarking for intentional browsing"
+            logo="/images/semble-logo.png"
+            year={2025}
+            status="ongoing"
+            githubLink="https://github.com/cosmik-network/semble"
+            backgroundImage={SembleScreenshot.src}
+            projectLink="https://semble.so/"
+          />
+        </Tabs.Panel>
+        <Tabs.Panel value="archived">
+          <ProjectCard
+            name="Hyperfeed"
+            description="Next-Generation Research Discovery"
+            logo="/images/hyperfeed-logo.png"
+            year={2024}
+            status="archived"
+            githubLink="https://github.com/Common-SenseMakers/sensemakers"
+            backgroundImage={HyperfeedScreenshot.src}
+            projectLink="/hyperfeed"
+          />
+        </Tabs.Panel>
       </Tabs>
     </Stack>
   );
