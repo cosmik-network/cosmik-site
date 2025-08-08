089eb67138992efc99827570e54f79f548d96bb2
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-08 13:38:27 -0700

    feat: project status tabs

--- src/containers/projectsContainer/ProjectsContainer.tsx
+++ src/containers/projectsContainer/ProjectsContainer.tsx
@@ -1,25 +1,48 @@
+"use client";
+
 import ProjectCard from "@/components/contentDisplay/projectCard/ProjectCard";
-import { Group } from "@mantine/core";
+import { Group, Stack, Tabs, Text } from "@mantine/core";
 
 export default function ProjectsContainer() {
   return (
-    <Group justify="center" align="stretch">
-      <ProjectCard
-        name="Semble"
-        description="Social bookmarking for intentional browsing."
-        logo="/images/semble-logo.png"
-        year={2025}
-        status="ongoing"
-        githubLink="https://github.com/cosmik-network/semble"
-      />
-      <ProjectCard
-        name="Hyperfeed"
-        description="Next-Generation Research Discovery."
-        logo="/images/hyperfeed-logo.png"
-        year={2024}
-        status="archived"
-        githubLink="https://github.com/Common-SenseMakers/sensemakers"
-      />
-    </Group>
+    <Stack align="center">
+      <Tabs variant="pills" radius="xl" defaultValue="ongoing">
+        <Tabs.List justify="center">
+          <Tabs.Tab value="ongoing">
+            <Text fz={"sm"} fw={600}>
+              Ongoing
+            </Text>
+          </Tabs.Tab>
+          <Tabs.Tab value="archived">
+            <Text fz={"sm"} fw={600}>
+              Archived
+            </Text>
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Group justify="center" align="stretch" mt={"lg"}>
+          <Tabs.Panel value="ongoing">
+            <ProjectCard
+              name="Semble"
+              description="Social bookmarking for intentional browsing."
+              logo="/images/semble-logo.png"
+              year={2025}
+              status="ongoing"
+              githubLink="https://github.com/cosmik-network/semble"
+            />
+          </Tabs.Panel>
+          <Tabs.Panel value="archived">
+            <ProjectCard
+              name="Hyperfeed"
+              description="Next-Generation Research Discovery."
+              logo="/images/hyperfeed-logo.png"
+              year={2024}
+              status="archived"
+              githubLink="https://github.com/Common-SenseMakers/sensemakers"
+            />
+          </Tabs.Panel>
+        </Group>
+      </Tabs>
+    </Stack>
   );
 }
