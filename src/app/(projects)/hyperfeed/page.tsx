7ff6d2702e7e6d69be009b0826f9d6ce02840881
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-08 17:08:06 -0700

    feat: hyperfeed page (minus image)

---
+++ src/app/(projects)/hyperfeed/page.tsx
@@ -0,0 +1,15 @@
+import { Container, Stack, Title } from "@mantine/core";
+import Hyperfeed from "@/app/content/hyperfeed/hyperfeed.mdx";
+
+export default function Page() {
+  return (
+    <Container size={"sm"}>
+      <Stack gap={"lg"}>
+        <Title order={1} fz={"3rem"}>
+          Hyperfeed: an AI-enhanced, cross platform research feed
+        </Title>
+        <Hyperfeed />
+      </Stack>
+    </Container>
+  );
+}
