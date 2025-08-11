ac5ea61d517bb1e7c030ee4dc76bc2ad05c984b7
Pouria Delfanazari <pdelfan@gmail.com>
2025-08-11 15:20:29 -0700

    feat: hyperfeed header image

--- src/app/(projects)/hyperfeed/page.tsx
+++ src/app/(projects)/hyperfeed/page.tsx
@@ -1,15 +1,27 @@
-import { Container, Stack, Title } from "@mantine/core";
+import { Container, Stack, Title, Image } from "@mantine/core";
 import Hyperfeed from "@/app/content/hyperfeed/hyperfeed.mdx";
+import HyperfeedHeaderImage from "@/assets/images/hyperfeed-header.webp";
 
 export default function Page() {
   return (
-    <Container size={"sm"}>      
-      <Stack gap={"lg"}>
-        <Title order={1} fz={"3rem"}>
-          Hyperfeed: an AI-enhanced, cross platform research feed
-        </Title>
-        <Hyperfeed />
-      </Stack>
-    </Container>
+    <Stack gap={"xl"}>
+      <Image
+        src={HyperfeedHeaderImage.src}
+        alt={
+          "Screenshot of Hyperfeed, showing the timeline, as well as a card view"
+        }
+        h={500}
+        w={1000}
+        mx={"auto"}
+      />
+      <Container size={"sm"}>
+        <Stack gap={"lg"}>
+          <Title order={1} fz={"3rem"}>
+            Hyperfeed: an AI-enhanced, cross platform research feed
+          </Title>
+          <Hyperfeed />
+        </Stack>
+      </Container>
+    </Stack>
   );
 }
